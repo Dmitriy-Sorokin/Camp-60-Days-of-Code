e53b11e6004b43c944d35b0baf454176feb6a1fb
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-07-06 12:43:47 +0300

    day 12

--- day_12/main_12.py
+++ day_12/main_12.py
@@ -1,10 +1,29 @@
-user_input = input("Enter feet and inches: ")
+# user_input = input("Enter feet and inches: ")
+#
+# list_user = user_input.split()
+#
+#
+# def converter(user_inp1, user_inp2):
+#     return user_inp1 * 0.3048 + user_inp2 * 0.0254
+#
+#
+# print(converter(float(list_user[0]), float(list_user[1])))
 
-list_user = user_input.split()
+# def check_password(user_pass):
+#     if len(user_pass) >= 8 and any(char.isdigit() for char in user_pass) and \
+#             any(char.isupper() for char in user_pass):
+#         return "Strong password"
+#     else:
+#         return "Eazy password"
+#
+# print(check_password("asfasf8S"))
+
+# def checker(list_user: list):
+#     final = sum(list_user) / len(list_user)
+#     return final
 
 
-def converter(user_inp1, user_inp2):
-    return user_inp1 * 0.3048 + user_inp2 * 0.0254
+def input_string(name: str):
+    return name.title()
 
-
-print(converter(float(list_user[0]), float(list_user[1])))
+print(input_string("jo"))

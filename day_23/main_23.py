e55b54355fcdcbe0716f9f79cde74b38b4add250
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-08-02 19:48:48 +0300

    day_23

---
+++ day_23/main_23.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    with open(r"c:\local git reposiroty\Camp-60-Days-of-Code\day_23\images\photo.png", "rb") as f:
+        image = f.read()
+    st.image(image)
+
+with col2:
+    st.title("Dima")
+    content = """
+    Python is a popular high-level programming language known for its simplicity and readability.
+    It is widely used for web development, data analysis, artificial intelligence, and more. 
+    Python's extensive library ecosystem and vibrant community make it a versatile choice for both beginners 
+    and experienced developers. With its clean syntax and emphasis on code readability, 
+    Python is a great language for anyone looking to start their journey into programming.
+    """
+    st.info(content)
+
+content2 = """
+Bellow you can find some of apps I have built in Python.
+"""
+
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv(r"c:\local git reposiroty\Camp-60-Days-of-Code\day_23\data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        with open(rf"c:\local git reposiroty\Camp-60-Days-of-Code\day_23\images\{row['image']}", "rb") as f:
+            image = f.read()
+        st.image(image)
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        with open(rf"c:\local git reposiroty\Camp-60-Days-of-Code\day_23\images\{row['image']}", "rb") as f:
+            image = f.read()
+        st.image(image)
+        st.write(f"[Source Code]({row['url']})")

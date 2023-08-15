1f8a9ab60848229bb6eb09c4e191502bf4c96af2
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-08-15 21:58:48 +0300

    day 31

--- day_29/main_29.py
+++ day_29/main_29.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+stations = pandas.read_csv("files/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
@@ -17,6 +21,23 @@ def about(station, date):
             "date": date,
             "temp": temperature}
 
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = f"files/TG_STAID{str(station).zfill(6)}.txt"
+    df = pandas.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = f"files/TG_STAID{str(station).zfill(6)}.txt"
+    df = pandas.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    print(result)
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True)
